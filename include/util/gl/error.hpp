673e0294f0c86ec96160faf673f7c4cf9d9abade
Mark van de Ruit <markvanderuit@gmail.com>
2021-07-28 23:08:14 +0200

    Implemented detailed exception

---
+++ include/util/gl/error.hpp
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Mark van de Ruit (Delft University of Technology)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <glad/glad.h>
+#include "util/error.hpp"
+
+namespace hdi::util {
+  inline
+  std::string glReadableError(GLenum err) {
+    switch (err) {
+    case GL_INVALID_ENUM:
+      return "GL_INVALID_ENUM";
+    case GL_INVALID_VALUE:
+      return "GL_INVALID_VALUE";
+    case GL_INVALID_OPERATION:
+      return "GL_INVALID_OPERATION";
+    case GL_STACK_OVERFLOW:
+      return "GL_STACK_OVERFLOW";
+    case GL_STACK_UNDERFLOW:
+      return "GL_STACK_UNDERFLOW";
+    case GL_OUT_OF_MEMORY:
+      return "GL_OUT_OF_MEMORY";
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+      return "GL_INVALID_FRAMEBUFFER_OPERATION";
+    case GL_CONTEXT_LOST:
+      return "GL_CONTEXT_LOST";
+    case GL_NO_ERROR:
+      return "GL_NO_ERROR";
+    default:
+      return "unknown";
+    }
+  }
+
+  namespace detail {
+    inline
+    void glAssertImpl(const char *msg, const char *file, int line) {
+      GLenum err;
+      while ((err = glGetError()) != GL_NO_ERROR) {
+        RuntimeError error("OpenGL error");
+        error.code = glReadableError(err);
+        error.file = file;
+        error.line = line;
+        throw error;
+      }
+    }
+  }
+} // hdi::util
+
+// Simple OpenGL assert with message, file name and line nr. attached. Throws RuntimeError
+#define glAssert(message) dh::util::detail::glAssertImpl(message, __FILE__, __LINE__);
