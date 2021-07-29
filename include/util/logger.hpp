77ab411391d99a7c7c2fd9bed2d5cd6abfa2b353
Mark van de Ruit <markvanderuit@gmail.com>
2021-07-29 22:20:55 +0200

    Implemented date logging, log refactor

--- include/util/logger.hpp
+++ include/util/logger.hpp
@@ -28,42 +28,74 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <date/date.h>
 
 namespace dh::util {
   class Logger {
   public:
-    virtual void log(const std::string &str) const = 0;
+    virtual void writeLine(const std::string &str) const = 0;
     virtual void flush() const = 0;
-
-    template <typename Value, bool Flush = true>
-    void logValue(const std::string &str, Value value) const {
-      std::stringstream ss;
-      ss << str << "\t:\t" << std::to_string(value);
-      log(ss.str());
-      if constexpr (Flush) {
-        flush();
-      }
-    }
-
-    template <typename Duration = std::chrono::milliseconds, bool Flush = true>
-    void logTime(const std::string &str, Duration duration) const {
-      std::stringstream ss;
-      ss << str << "\t:\t" << std::to_string(duration.count());
-      log(ss.str());
-      if constexpr (Flush) {
-        flush();
-      }
-    }
   };
 
   class CoutLogger : public Logger {
   public:
-    void log(const std::string &str) const override {
+    virtual void writeLine(const std::string &str) const override {
       std::cout << str << '\n';
     }
     
-    void flush() const override {
+    virtual void flush() const override {
       std::cout << std::flush;
     }
   };
+
+  class DateCoutLogger : public CoutLogger {
+  public:
+    virtual void writeLine(const std::string &str) const override {
+      using namespace date;
+      using namespace std::chrono;
+
+      std::cout << floor<milliseconds>(system_clock::now())
+                << " " << str << '\n';
+    }
+  };
+  
+  template <class Logger, bool flush = false>
+  inline
+  void log(Logger* ptr, const std::string& str) {
+    if (ptr) {
+      ptr->writeLine(str);
+      if constexpr (flush) {
+        ptr->flush();
+      }
+    }
+  }
+
+  template <class Logger, typename T, bool flush = false>
+  inline
+  void logValue(Logger* ptr, const std::string& str, T t) {
+    if (ptr) {
+      std::stringstream ss;
+      ss << str << " : " << std::to_string(value);
+      ptr->writeLine(ss.str());
+      if constexpr (flush) {
+        ptr->flush();
+      }
+    }
+  }
+
+  template <class Logger, typename Duration = std::chrono::milliseconds, bool flush = false>
+  inline
+  void logTime(Logger* ptr, const std::string& str, Duration duration) {
+    using namespace date;
+    using namespace std::chrono;
+
+    if (ptr) {
+      std::stringstream ss;
+      ss << str << " : " << duration;
+      ptr->writeLine(ss.str());
+      if constexpr (flush) {
+        ptr->flush();
+      }
+    }
+  }
 } // dh::util
